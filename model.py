aabdd61d73de2b671dcc4f5c7c0a297857eec56a
fenira that't it <108204426+ambufenn@users.noreply.github.com>
2025-11-18 22:28:55 +0700

    Update model.py

--- model.py
+++ model.py
@@ -1,1 +1,6 @@
-gdsgdsgdfg
+import os
+import google.generativeai as genai
+
+def load_model():
+    genai.configure(api_key=os.environ["GEMINI_API_KEY"])
+    return genai.GenerativeModel("gemini-1.5-flash")
